f190002ba0c4d26752f5c46cbf8a862cd6e65fe4
Will Orr <will@worrbase.com>
2013-04-24 21:35:06 -0400

    Wrote starter for securing the mail form a little bit

--- mail_form.php
+++ mail_form.php
@@ -1,7 +1,21 @@
 <?php
+	$EMAIL_ADDRESSES = 
+		array(
+			"chairman@csh.rit.edu",
+			"eval@csh.rit.edu",
+			"history@csh.rit.edu",
+			"scholarship@csh.rit.edu",
+			"webmaster@csh.rit.edu"
+		);
+
+
+	$recipient = $_POST['sendtoemail'];
+	if (! in_array($recipient, $EMAIL_ADDRESSES, TRUE)) {
+		die("Redirect to an error page");
+	}
+
 	$name = $_POST['f1'];
 	$email = $_POST['f2'];
-	$recipient = $_POST['sendtoemail'];
 	$message = $_POST['f3'];
 	$formcontent="From: $name \nEmail: $email \nMessage: $message";
 	$subject = "A message from the public website";
