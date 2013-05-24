7f47ce53503423bfa7b03dd1e87e98f1eb3a0179
William Orr <will@worrbase.com>
2013-05-24 11:02:04 -0700

    Fixed Ben's original submission and submitted it
    - No Clockspam
    - Fuck PHP

--- mail_form.php
+++ mail_form.php
@@ -8,18 +8,29 @@
 			"webmaster@csh.rit.edu"
 		);
 
-
+	// Verify that we're sending our message to a valid email
 	$recipient = $_POST['sendtoemail'];
 	if (! in_array($recipient, $EMAIL_ADDRESSES, TRUE)) {
-		die("Redirect to an error page");
+		header("Location: failure.php");
+		exit;
 	}
 
-	$name = $_POST['f1'];
-	$email = $_POST['f2'];
+	// Replace newlines to prevent header insertion
+	$name = preg_replace('/\R/u', '', $_POST['f1']);
+	$email = preg_replace('/\R/u', '', $_POST['f2']);
+
+	// Validate email, this is probably less than great
+	$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+	if (!email) {
+		header("Location: failure.php");
+		exit;
+	}
+
 	$message = $_POST['f3'];
 	$formcontent="From: $name \nEmail: $email \nMessage: $message";
 	$subject = "A message from the public website";
 	$mailheader = "From: $email \r\n";
+
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 	header("Location: thankyou.php");
 ?>
