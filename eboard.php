454ff809d46d0ff547c626d0899e133bc13faff7
Matt Gambogi <m@gambogi.com>
2013-07-15 18:15:06 -0400

    Fixed (imho) various grammar, phrasing, and oversharing issues.

--- eboard.php
+++ eboard.php
@@ -19,7 +19,7 @@
 					<h2>Chairman</h2>
 					<h3>Josh McSavaney</h3>
 					<p>
-						The Chairman of CSH serves as the &ldquo;head of E-Board&rdquo;, supervising the activities of the Directors and presiding over our weekly House Meetings. The Chairman is also the primary spokesperson for House, representing the interests of our members in communications with RIT and the rest of the world. The chairman also casts the tie-breaking vote on E-Board decisions, if needed.
+						The Chairman of CSH serves as the head of E-Board, supervising the activities of the directors and presiding over our weekly house meetings. The Chairman is also the primary spokesperson for the House, representing the interests of our members in communications with RIT and the rest of the world.
 					</p>
 				</span>
 			</li>
@@ -30,7 +30,7 @@
 					<h2>Evaluations</h2>
 					<h3>Brian Lewis</h3>
 					<p>
-						&rdquo;Eval&ldquo; is responsible for connecting with the RIT student community, as well as screening, accepting, and orienting new members. Eval is why current members are here today!
+						The Evaluations director (Eval) is responsible for connecting with the RIT student community, as well as screening, accepting, and orienting new members. Eval is why current members are here today!
 						<br/><br/>
 						If you would like to visit or tour the floor, let the Eval Director know and he can arrange this.
 				</p>
@@ -43,7 +43,7 @@
 					<h2>Financial</h2>
 					<h3>Jeff Haak</h3>
 					<p>
-						Financial is responsible for managing CSH's finances, collecting semesterly member dues, and hatching new fundraising schemes. All of our money goes towards new resources and project components for members to use; CSH is a registered non-profit organization with an annual self-generated budget.
+						The Financial director is responsible for managing CSH's finances, collecting semesterly member dues, and hatching new fundraising schemes. All of our money goes towards new resources and project components for members to use; CSH is a registered non-profit organization with an annual self-generated budget.
 					</p>
 				</span>
 			</li>
@@ -54,7 +54,7 @@
 					<h2>House History</h2>
 					<h3>Josh Winemiller</h3>
 					<p>
-						History's primary responsibility is keeping the line of communication between CSH and our alumni open. We believe members should have a well-developed sense of House's origins and accomplishments. History keeps the past alive through the yearbook, Coredump newsletter, floor displays, archiving, updating the media database, and managing alumni relations. History also coordinates alumni storytelling nights and other traditional events.
+						The primary responsibility of the History director (History) is keeping the line of communication between CSH and our alumni open. We believe members should have a well-developed sense of House's origins and accomplishments. History keeps the past alive through the yearbook, Coredump newsletter, floor displays, archiving, updating the media database, and managing alumni relations. History also coordinates alumni storytelling nights and other traditional events.
 					</p>
 				</span>
 			</li>
@@ -65,7 +65,7 @@
 					<h2>House Improvements</h2>
 					<h3>Stephen Demos</h3>
 					<p>
-						&rdquo;Imps&ldquo; is responsible for keeping our living space respectable and comfortable. The Imps director delegates projects that improve the physical aspects of floor, such as painting, cleaning, building, and organizing House's resources. While we may not be the tidiest group of people, we like to take care of our floor, and it shows.
+						The House Improvements (Imps) director is responsible for keeping our living space respectable and comfortable. The Imps director delegates projects that improve the physical aspects of floor, such as painting, cleaning, building, and organizing House's resources. While we may not be the tidiest group of people, we like to take care of our floor, and it shows.
 					</p>
 				</span>
 			</li>
@@ -87,7 +87,7 @@
 					<h2>Research and Development</h2>
 					<h3>Ben Centra</h3>
 					<p>
-						"R&amp;D" is responsible for organizing seminars and assisting with technical projects, with the goal of encouraging members to learn new skills and gain experience.
+						R&amp;D is responsible for organizing seminars and assisting with technical projects, with the goal of encouraging members to learn new skills and gain experience.
 					</p>
 				</span>
 			</li>
@@ -98,7 +98,7 @@
 					<h2>Social</h2>
 					<h3>Reed Swiernik &amp; Tal Cohen</h3>
 					<p>
-						Social is responsible for planning and running social events for our members, such as diner runs, movie nights, project work weekends, and more.
+						The Social directors (Social) are responsible for planning and running social events for our members, such as diner runs, movie nights, project work weekends, and more.
 					</p>
 				</span>
 			</li>
