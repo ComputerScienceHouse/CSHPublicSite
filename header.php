1976c287f8e4fd2f21e9db60e5dec0bbf3a98123
Michail Yasonik <michail@yasonik.com>
2013-08-19 11:44:11 -0400

    fixed GA to send data to CSH's GA account

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-40309079-1', 'rit.edu');
+			ga('create', 'UA-75959-4', 'rit.edu');
 			ga('send', 'pageview');
 		</script>
 	</head>
