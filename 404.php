ea89f08eb87a70f581bbfbb3a7ef86b980837916
Andrew Hanes <ahanes@csh.rit.edu>
2014-11-20 11:35:17 -0500

    new 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php include "header.php"; ?>
+<div id="big">
+	<div class="wrapper">
+		<div id="left">
+			<p id="fitText">
+				404: Not Found
+			</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/t3otBjVZzT0" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+<script>
+	$('#fitText').fitText(1.25, {minFontSize:24});
+</script>
+<?php include "footer.php"; ?>
