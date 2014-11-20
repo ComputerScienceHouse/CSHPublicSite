ef6cc572774b4cbb4d0a67310260f90a49f74caf
Andrew Hanes <ahanes@csh.rit.edu>
2014-11-20 13:55:35 -0500

    fix for mobile

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 			<p id="fitText">
 				404: Not Found
 			</p>
-<iframe width="560" height="315" src="//www.youtube.com/embed/t3otBjVZzT0" frameborder="0" allowfullscreen></iframe>
+<iframe width="50%" height="315" src="//www.youtube.com/embed/t3otBjVZzT0" frameborder="0" allowfullscreen></iframe>
 		</div>
 	</div>
 </div>
