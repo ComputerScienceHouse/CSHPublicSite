94e91f8e2c42eb0112453b76bc6c26ba5b20b637
Reed Swiernik <rswiernik@csh.rit.edu>
2015-02-02 22:26:37 -0500

    Updated eboard names.

--- eboard.php
+++ eboard.php
@@ -52,7 +52,7 @@
 				<div class="rolloverImg" id="historyImg"><img src="images/eboard/history.jpg" /></div>
 				<span>
 					<h2>House History</h2>
-					<h3>Matt Gambogi</h3>
+					<h3>Josh McSavaney</h3>
 					<p>
 						The primary responsibility of the History director (History) is keeping the line of communication between CSH and our alumni open. We believe members should have a well-developed sense of House's origins and accomplishments. History keeps the past alive through the yearbook, Coredump newsletter, floor displays, archiving, updating the media database, and managing alumni relations. History also coordinates alumni storytelling nights and other traditional events.
 					</p>
@@ -74,7 +74,7 @@
 				<div class="rolloverImg" id="opCommImg"><img src="images/eboard/opcomm.jpg" /></div>
 				<span>
 					<h2>OpComm</h2>
-					<h3>Josh McSavaney</h3>
+					<h3>Ross Delinger</h3>
 					<p>
 						The OpComm director, together with a group of technically skilled members known as RTPs, is responsible for maintaining CSH's servers and network infrastructure. Becoming an RTP allows members to develop and practice skills that are essential for working in computing fields. If you have any questions about House's technical resources, the OpComm director should be your first stop.
 					</p>
