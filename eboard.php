15e3c036052563a4fc01e9a0e631095256119180
Marc Billow <mbillow@users.noreply.github.com>
2015-05-30 19:37:48 -0400

    Corrected Henry's name.

--- eboard.php
+++ eboard.php
@@ -96,7 +96,7 @@
 				<div class="rolloverImg" id="socialImg"><img src="images/eboard/social.jpg" /></div>
 				<span>
 					<h2>Social</h2>
-					<h3>Henry Sanuik and Rosalee Hacker</h3>
+					<h3>Henry Saniuk and Rosalee Hacker</h3>
 					<p>
 						The Social directors (Social) are responsible for planning and running social events for our members, such as diner runs, movie nights, project work weekends, and more.
 					</p>
