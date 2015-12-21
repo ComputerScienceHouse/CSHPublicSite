5213e534097db342f4ff61a58a8e388b216f0368
Andrew Hanes <ahanes@csh.rit.edu>
2015-12-21 17:10:07 -0500

    clean up html, better title

--- 40th.php
+++ 40th.php
@@ -1,25 +1,29 @@
 <?php include "header.php"; ?>
 <div id="big" class="title">
 <div class="wrapper">
-</div>
+		<h1>
+			40th Information
+		</h1>
+	</div>
 </div>
 <div id="content">
-<div id="cWrapper"  />
-<div class="40th_docs" style="float: right; width 30%;">
-<h2>Quick Links</h2>
-<br />
-<ul>
-<li><a href="40th_chairman_letter.php">Chairman Letter</a></li>
-<li><a href="40th_save_the_date.php">Save the Date</a></li>
-</ul>
-</div>
-<h2>40th News</h2>
-<div class="40thupdate" style="float: left; width: 70%;">
-<br />
-<h5><p>12/04/2015</p></h5>
-<p class="40th_info">Recently added the <a href="40th_chairman_letter.php">letter from our chairman</a> Andrew Glaude.  This letter gives a high level overview of the event including activities and the formal dinner.</p>
-</div>
-</div>
+   <div id="cWrapper"  />
+      <div class="40th_docs" style="float: right; width 30%;">
+         <h2>Quick Links</h2>
+         <br />
+         <ul>
+            <li><a href="40th_chairman_letter.php">Chairman Letter</a></li>
+            <li><a href="40th_save_the_date.php">Save the Date</a></li>
+         </ul>
+      </div>
+      <div class="40thupdate" style="float: left; width: 70%;">
+         <br />
+         <h5>
+            <p>12/04/2015</p>
+         </h5>
+         <p class="40th_info">Recently added the <a href="40th_chairman_letter.php">letter from our chairman</a> Andrew Glaude.  This letter gives a high level overview of the event including activities and the formal dinner.</p>
+      </div>
+   </div>
 </div>
 </div>
 <?php include "footer.php"; ?>
