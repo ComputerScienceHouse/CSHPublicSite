66016bdc71337ef297424a0fc266361ba2a0f8ea
Bradon Hudson <bhud07@gmail.com>
2016-02-04 19:50:41 -0500

    PyBuilder Update
    Updates PyBuilder to latest version - now copies all asset directories
    upon building and places them into the build/ directory.

--- pyBuilder.py
+++ pyBuilder.py
@@ -2,6 +2,38 @@ import glob
 import codecs
 import os 
 import sys
+import shutil
+import errno
+
+
+def walklevel(some_dir, level):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+"""
+* Copies all additional directories when buiding all
+* Params:
+*  - src - string - string to source dir
+*  - dest - string - string to destination dir
+* Returns: None
+"""
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        if "LICENSE" not in item and "README" not in item and "pyBuilder.py" not in item:
+            s = os.path.join(src, item)
+            if "build" not in s and ".git" not in s:
+                d = os.path.join(dst, item)
+                if os.path.isdir(s):
+                    shutil.copytree(s, d, symlinks, ignore)
+                else:
+                    shutil.copy2(s, d)
+
 
 """
 * Builds the HTML
@@ -10,8 +42,6 @@ import sys
 """
 def buildFile(fileNameIn,fileIn):
     print("building "+fileNameIn+"...") #debug
-    if not os.path.exists("build"):
-            os.makedirs("build")
     fileNameDir = "build/"+fileNameIn;
     try:
         os.remove(fileNameDir)
@@ -70,7 +100,22 @@ def main():
     print("================")
     print()
     if(len(sys.argv) > 2):
+        if not os.path.exists("build"):
+            os.makedirs("build")
+        else:
+            shutil.rmtree('build')
+            os.makedirs("build")
         
+        
+        # Copy all assets over to build directory #
+        workingDir = os.getcwd()
+        print("Copying all assets...")
+        for root, dirs, files in walklevel(os.getcwd(),0):
+                copytree(root,workingDir+"/build/")
+          
+        print("Assets copied to build directory.")
+        print()
+        # Generate HTML #
         fileArray = glob.glob("*.html");
         for i in range (0,len(fileArray)):
             fileName = fileArray[i]
