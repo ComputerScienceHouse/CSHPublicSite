56be60eac81f62e0ba30327508e2c39222ebbd78
Steven Mirabito <steven@stevenmirabito.com>
2016-02-08 12:49:31 -0500

    Fix carousel and implement workaround for show/hide bug

--- resources/js/tour.js
+++ resources/js/tour.js
@@ -1,5 +1,9 @@
 // Tour Image Carousel JS
 
+$.get("resources/templates/_carouselPrototype.html", function (data) {
+        $.carouselPrototype = data;
+
+    });
 $.get("resources/templates/_loungeCarousel.html", function (data) {
         $.lounge = data;
     
@@ -39,50 +43,67 @@ $.get("resources/templates/_loungeCarousel.html", function (data) {
 // Return: None
 function switchImage(room){
     var imageHTML;
+    var containerElement = $('#carouselImages');
     $('.tourTextActive').hide();
     $('.tourTextActive').removeClass('tourTextActive');
-    if(room == "lounge"){
-        imageHTML = $.lounge;
 
+    if(room == "server"){
+        imageHTML = $.server;
+        
+        // Show Photosphere container
+        containerElement = $('#photosphere-container');
+        containerElement.show();
+
+        // Photosphere, remove carousel
+        // Part of the workaround for the carousel bug (see below)
+        $('.carousel').remove();
+    } else {
+        // Hide photosphere container
+        $('#photosphere-container').hide();
+
+        if(room == "lounge"){
+                imageHTML = $.lounge;
+
+        }
+        else if(room == "software"){
+                imageHTML = $.software;
+
+        }
+        else if(room == "library"){
+                imageHTML = $.library;
+
+        }
+        else if(room == "user"){
+                imageHTML = $.user;
+
+        }
+        else if(room == "conference"){
+                imageHTML = $.conference;
+
+        }
+        else if(room == "project"){
+               imageHTML = $.project; 
+
+        }
+        else if(room == "dorm"){
+                imageHTML = $.dorm;
+
+        }
     }
-    else if(room == "software"){
-            imageHTML = $.software;
-
-    }
-    else if(room == "library"){
-            imageHTML = $.library;
-
-    }
-    else if(room == "user"){
-            imageHTML = $.user;
-
-    }
-    else if(room == "conference"){
-            imageHTML = $.conference;
-
-    }
-    else if(room == "server"){
-            imageHTML = $.server;
-
-            // Photosphere instead of carousel, hide controls
-            $('.carousel-control').hide();
-
-    }
-    else if(room == "project"){
-           imageHTML = $.project; 
-
-    }
-    else if(room == "dorm"){
-            imageHTML = $.dorm;
-
-    }
+    
     $('.csh-active').removeClass('csh-active');
     $("#"+room+"Text").show();
     $("#"+room+"Text").addClass("tourTextActive");
-    $('#carouselImages').html(imageHTML);
+
+    // If the carousel doesn't exist in the DOM, add it
+    // Workaround for this bug: https://github.com/angular-ui/bootstrap/issues/1513
+    if (room !== 'server' && $('.carousel').length == 0) {
+        $('.image-container').append($.carouselPrototype.replace('{{images}}', imageHTML));
+        $('.carousel').carousel();
+    } else {
+        containerElement.html(imageHTML);
+    }
     $('#'+room).addClass('csh-active');
-    
-    
     
     
 }
