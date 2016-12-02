b41d0b7cf25fe86ae002afc5cd317119733f8a57
Reed Swiernik <reed.swiernik@gmail.com>
2016-12-01 23:24:55 -0800

    Fix akath20 Merge

--- resources/js/sponsors.js
+++ resources/js/sponsors.js
@@ -3,48 +3,46 @@
 
 $(document).ready(function(){
     var images = [
-        'resources/images/sponsorLogos/ritLarge.png',
-        'resources/images/sponsorLogos/symantec.png',
-        'resources/images/sponsorLogos/att.png',
-        'resources/images/sponsorLogos/datto.png',
-        'resources/images/sponsorLogos/UofR.png',
-        'resources/images/sponsorLogos/linkedin.png',
-        'resources/images/sponsorLogos/microsoft.png',
-        'resources/images/sponsorLogos/cisco.png',
-        'resources/images/sponsorLogos/google.png',
-        'resources/images/sponsorLogos/hp.png',
-        'resources/images/sponsorLogos/johnsonjohnson.png',
-        'resources/images/sponsorLogos/lourdesindustries.png',
-        'resources/images/sponsorLogos/mooreresearchcenter.png',
-        'resources/images/sponsorLogos/intergraph.png',
-        'resources/images/sponsorLogos/nortel.png',
-        'resources/images/sponsorLogos/janestreet.png',
-        'resources/images/sponsorLogos/northropgrumman.png',
-        'resources/images/sponsorLogos/sonus.png',
-        'resources/images/sponsorLogos/oracle.png',
-        'resources/images/sponsorLogos/perforce.png',
-        'resources/images/sponsorLogos/cadence.png',
-        'resources/images/sponsorLogos/podi.png',
-        'resources/images/sponsorLogos/dupont.png',
-        'resources/images/sponsorLogos/amdex.png',
-        'resources/images/sponsorLogos/ti.png',
-        'resources/images/sponsorLogos/xerox.png',
-        'resources/images/sponsorLogos/citrix.png',
-        'resources/images/sponsorLogos/fxcm.png',
-        'resources/images/sponsorLogos/pdhi.png' 
+        ['resources/images/sponsorLogos/ritLarge.png', "http://rit.edu"],
+        ['resources/images/sponsorLogos/symantec.png', "http://symantec.com" ],
+        ['resources/images/sponsorLogos/att.png', "http://att.com" ],
+        ['resources/images/sponsorLogos/datto.png', "http://datto.com" ],
+        ['resources/images/sponsorLogos/UofR.png', "http://rochester.edu" ],
+        ['resources/images/sponsorLogos/linkedin.png', "http://linkedin.com" ],
+        ['resources/images/sponsorLogos/microsoft.png', "http://microsoft.com" ],
+        ['resources/images/sponsorLogos/cisco.png', "http://cicso.com" ],
+        ['resources/images/sponsorLogos/google.png', "http://googlec.ocom" ],
+        ['resources/images/sponsorLogos/hp.png', "http://hp.com" ],
+        ['resources/images/sponsorLogos/johnsonjohnson.png', "http://jnj.com" ],
+        ['resources/images/sponsorLogos/lourdesindustries.png', "http://lourdesinc.com" ],
+        ['resources/images/sponsorLogos/mooreresearchcenter.png', "http://mrci.com" ],
+        ['resources/images/sponsorLogos/intergraph.png', "http://intergraph.com" ],
+        ['resources/images/sponsorLogos/nortel.png', "http://nortel-us.com" ],
+        ['resources/images/sponsorLogos/janestreet.png', "http://janestreet.com" ],
+        ['resources/images/sponsorLogos/northropgrumman.png', "http://northropgrumman.com" ],
+        ['resources/images/sponsorLogos/sonus.png', "http://sonus.net" ],
+        ['resources/images/sponsorLogos/oracle.png', "http://oracle.com" ],
+        ['resources/images/sponsorLogos/perforce.png', "http://perforce.com" ],
+        ['resources/images/sponsorLogos/cadence.png', "http://cadence.com" ],
+        ['resources/images/sponsorLogos/podi.png', "http://podi.com" ],
+        ['resources/images/sponsorLogos/dupont.png', "http://dupont.com" ],
+        ['resources/images/sponsorLogos/amdex.png', "http://amdexcorp.com" ],
+        ['resources/images/sponsorLogos/ti.png', "http://ti.com" ],
+        ['resources/images/sponsorLogos/xerox.png', "http://xerox.com" ],
+        ['resources/images/sponsorLogos/citrix.png', "http://citrix.com" ],
+        ['resources/images/sponsorLogos/fxcm.png', "http://fxcm.com" ],
+        ['resources/images/sponsorLogos/pdhi.png', "http://pdhi.com" ],
     ];
-    
+
     for(i=0;i<29;i++){
         var index = Math.floor(Math.random()*images.length)
         var item = images[index];
-        $('#sponsor'+i).attr("src", item);
+        $('#sponsor'+i).attr("src", item[0]);
+        $('#sponsor'+i).parent("a").attr("href", item[1]);
         images.splice(index,1);
-        
-        
-        
-        
+
     }
-    
-    
-    
+
+
+
 });
