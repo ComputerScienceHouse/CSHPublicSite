7b0f0da77bbbfae7daa78ee3f213b43935aa6030
Adam Brewer <adamhb321@gmail.com>
2021-09-09 08:14:19 -0400

    Remove max-height styling for slick carousel, add scroll down button
    Removes the max-height styling for the scrolling carousel.
    
    Adds the carousel scroll down button, which solves the design issue
    of images taking up the whole page (which upon first viewing might
    lead someone to believe that is all the page contains) by providing
    an indicator+button that scrolls to reveal the rest of the page.
    
    Upgrades jquery from slim to full. This is required so we can use
    animate for the smooth scrolling functionality.
    
    Moves all javascript libraries from the footer to header, if this
    is undesirable for any particular reason (I really didn't see any
    issues), just say so and I'll pull it from the PR.
    
    Resolves #280

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
-    if ($('#slider').length) {
-        $('#slider').slick({
+    let slider = $("#slider");
+    if (slider.length) {
+        slider.slick({
             dots: true,
             autoplay: true,
             autoplaySpeed: 5000,
@@ -8,4 +9,29 @@ $(document).ready(() => {
             swipe: true,
         });
     }
+    // Setup carousel scroll-down button click event
+    let carouselsdb = $("#carousel-scroll-down-button");
+    let scrollTopOffset = 1;
+    carouselsdb.on("click", () => {
+        $("html, body").animate({ scrollTop: slider.find("img").height() + scrollTopOffset - $("nav").outerHeight() }, "slow");
+        carouselsdb.blur();
+    });
+
+    // Position carousel scroll-down button
+    let carouselsdbOffset = {X: -7, Y: -35};
+    $(window).on("scroll",  () => {
+        let sliderBottom = slider.height() + slider.offset().top;
+        let windowBottom = $(window).scrollTop() + $(window).height();
+        if ($(window).scrollTop() > sliderBottom) {
+            carouselsdb.css("visibility", "hidden");
+        } else {
+            if(windowBottom < sliderBottom){
+                carouselsdb.css("transform", `translate3d(${-carouselsdb.width()/2 + carouselsdbOffset.X}px, ${windowBottom-carouselsdb.height() + carouselsdbOffset.Y}px,0px)`);
+            } else{
+                carouselsdb.css("transform", `translate3d(${-carouselsdb.width()/2 + carouselsdbOffset.X}px, ${slider.height()-carouselsdb.height() + carouselsdbOffset.Y}px, 0px)`);
+            }
+            carouselsdb.css("visibility", "visible");
+        }
+    }).scroll();
+    $(window).on("load resize focus blur",()=>$(window).scroll());
 });
